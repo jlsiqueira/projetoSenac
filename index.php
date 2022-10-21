8451b7e40c1448a922a34d2b6986dab3aa9aa3f2
jose luiz <joseluizebel@yahoo.com.br>
2022-10-20 21:52:10 -0400

    aula php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//$meuarray = array('valor1','valor2','valor3','valor x');
+// print_r($meuarray);
+//resultado = Array ([0] => valor1 [1] => valor2 [2], => valor3 [3], => valorx [x])
+
+$meuarray = array('teste','aula');
+print_r($meuarray);
+
+echo $meuArray[0];
+?>
--- indexController.php
+++ indexController.php
@@ -1,13 +1,34 @@
 <?php
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
+//$nome = $_POST['nome'];
+//$idade = $_POST['idade'];
 
-echo $nome; 
-echo "<br>";
+//echo $nome; 
+//echo "<br>";
 
-if($idade > 18){
-    echo " {$idade} Velho";
-}else{
-    echo " {$idade} Novo";
+//if($idade > 18){
+//    echo " {$idade} Velho";
+//}else{
+    // echo " {$idade} Novo";
+//}
+
+
+// $frutas = array('maca','banana','melancia','melao','abacaxi','laranja');
+// for($posicao = 0; $posicao < count($frutas); $posicao++)
+// {
+//     echo "A fruta na posição:" . $posicao . " é " . $frutas[$posicao] . " ";
+//     "<br>";
+// }
+
+
+$numeros = array('0','1','6','3','4','5');
+$verificador = $numeros[0];
+for($posicao = 0; $posicao < count($numeros); $posicao++){
+
+    if($verificador < $numeros[$posicao]){
+        $verificador = $numeros[$posicao];
+  }
+
 }
+
+echo "O maior número é: "  . $verificador. " ";
 ?>
